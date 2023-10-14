65351067a1afa0858a6e88e4b50c81f3621cb0ad
BuildTools <unconfigured@null.spigotmc.org>
2023-10-14 20:19:25 +0300

    init

--- Разделение игроков на две команды.py
+++
@@ -1,3 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-print(list_players[:int(len(list_players)/2):], "\n", list_players[int(len(list_players)/2)::], sep="")
-# TODO Разделите участников на две команды
